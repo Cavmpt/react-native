242da0d4513d5f65871794be8d0ef90d1cf81f0f
Cavmpt <keviny4n@gmail.com>
2021-06-30 12:32:25 -0400

    fixed tests as well as test messaging

--- src/components/Pages/Controls/__test__/Controls.test.tsx
+++ src/components/Pages/Controls/__test__/Controls.test.tsx
@@ -3,16 +3,22 @@
 import React from 'react'
 import {render, screen} from '@testing-library/react'
 import Controls from '../Controls'
+import {Context} from '../../../../store/Provider'
 
 describe('Alert Box behavior works as expected', () => {
   it('displays the threats correctly', async () => {
+    const currentThreats = [
+      {id: 1, message: 'Threat 1', value: '12341234123ff'},
+      {id: 2, message: 'Threat 2', value: '12341234123ff'},
+      {id: 3, message: 'Threat 3', value: '12341234123ff'},
+    ]
     const currentAlerts = [
       {id: 1, message: 'Alerts 1', value: '12341234123ff', acknowledged: false},
       {id: 2, message: 'Alerts 2', value: '12341234123ff', acknowledged: false},
       {id: 3, message: 'Alerts 3', value: '12341234123ff', acknowledged: false},
     ]
     render(
-      <Context.Provider value={{currentAlerts}}>
+      <Context.Provider value={{currentThreats, currentAlerts}}>
         <Controls />
       </Context.Provider>,
     )
--- src/components/UIcomponents/Sidebar/__test__/Sidebar.test.tsx
+++ src/components/UIcomponents/Sidebar/__test__/Sidebar.test.tsx
@@ -1,18 +1,15 @@
 import React from 'react'
-import {render, screen, fireEvent} from '@testing-library/react'
+import {render, screen} from '@testing-library/react'
+import {BrowserRouter} from 'react-router-dom'
 import SideBar from '../Sidebar'
 
-describe('Alert Box behavior works as expected', () => {
-  global.window = Object.create(window)
-  const url = 'http://localhost:3000/controls'
-  Object.defineProperty(window, 'location', {
-    value: {
-      href: url,
-    },
-  })
-  it('expect the sidebar to display the correct url', async () => {
-    render(<SideBar />)
-    fireEvent.click(screen.getByTestId('control-button'))
-    expect(window.location.href).toEqual(url)
+describe('Sidebar Renders', () => {
+  it('expect control button from the sidebar to be not null', async () => {
+    render(
+      <BrowserRouter>
+        <SideBar />
+      </BrowserRouter>,
+    )
+    expect(screen.getByTestId('control-button')).toBeTruthy()
   })
 })
