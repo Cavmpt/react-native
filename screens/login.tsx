d9213ef94ee3a4fce885070e96192358d5a52fde
cavmpt <keviny4n@gmail.com>
2021-08-02 11:48:05 -0600

    initial setup for the login page

---
+++ screens/login.tsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import { SafeAreaView, StyleSheet, TextInput, View, Text } from "react-native";
+
+interface Props {
+  
+}
+
+const Login = (props: Props) => {
+  return (
+    <SafeAreaView>
+      <TextInput
+        style={styles.input}
+        onChangeText={onChangeText}
+        value={text}
+      />
+      <TextInput
+        style={styles.input}
+        onChangeText={onChangeText}
+        value={text}
+      />
+      <TextInput
+        style={styles.input}
+        onChangeText={onChangeText}
+        value={text}
+      />
+    </SafeAreaView>
+  )
+}
+
+const styles = StyleSheet.create({
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+  },
+});
+
+export default Login
