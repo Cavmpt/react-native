385df303e2c81adf89af43ba0b1e237b0cbc7f58
cavmpt <keviny4n@gmail.com>
2021-08-05 15:53:16 -0400

    inital setup of login and signup page

--- screens/login.tsx
+++ screens/login.tsx
@@ -1,40 +1,66 @@
 import React, {useState} from 'react'
-import { SafeAreaView, StyleSheet, TextInput, View, Text } from "react-native";
+import {SafeAreaView, StyleSheet, TextInput, View, Text} from 'react-native'
 
-interface Props {
-
-}
+interface Props {}
 
 const Login = (props: Props) => {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
+  const [username, setUsername] = useState<string>('')
+  const [password, setPassword] = useState('')
+
+  const onChangeText = (value: string) => {
+    setUsername(value)
+  }
 
   return (
-
     <SafeAreaView style={styles.container}>
-      {/* <TextInput
-        style={styles.input}
-        onChangeText={onChangeText}
-        value={text}
+      <Text style={styles.xguard_title}>Xguard</Text>
+      <Text style={styles.xguard_subtitle}>Robotic</Text>
+      <TextInput
+        style={styles.input_username}
+        onChangeText={value => {
+          onChangeText(value)
+        }}
+        value={username}
       />
       <TextInput
-        style={styles.input}
-        onChangeText={onChangeText}
-        value={text}
+        style={styles.input_password}
+        onChangeText={value => {
+          onChangeText(value)
+        }}
+        value={username}
       />
-      <TextInput
-        style={styles.input}
-        onChangeText={onChangeText}
-        value={text}
-      /> */}
     </SafeAreaView>
   )
 }
 
 const styles = StyleSheet.create({
-  container : {
-    flex: 1
-  }
-});
+  container: {
+    flex: 1,
+    backgroundColor: 'white',
+    paddingTop: 10,
+  },
+  xguard_title: {
+    textAlign: 'center',
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+  xguard_subtitle: {
+    textAlign: 'center',
+    fontSize: 10,
+    fontWeight: 'bold',
+  },
+  input_username: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+  },
+  input_password: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+  },
+})
 
 export default Login
