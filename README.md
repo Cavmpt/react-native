54a25f523c5bd388c963d1d4bfd1f341ecc70939
cavmpt <keviny4n@gmail.com>
2021-08-10 15:33:01 -0400

    README update

---
+++ README.md
@@ -0,0 +1,21 @@
+# Getting started
+
+Expo Drone UI dashboard with typescript and React.
+
+### Reference Documentation
+
+Technologies & Notations:
+
+- [Official React documentation](https://reactjs.org/)
+
+# MORE TECHNOLOGIES AND NOTATION COMING TODO !
+
+# To run the project
+
+Please ensure that you have the node version 16.4.2<br>
+
+IOs emulator support in preferred in order to run this application<br>
+
+To change the node version [NVM](https://github.com/nvm-sh/nvm) <br>
+
+npm i expo <br> expo install<br> npm run expo
--- helpers/fetch-call-repo.ts
+++ helpers/fetch-call-repo.ts
@@ -1,10 +1,11 @@
 import { createReadableStream } from "./deserialize/deserialize-methods"
-import { createArrayBuffer } from "./deserialize/ deserialize-methods"
+import { createArrayBuffer } from "./deserialize/deserialize-methods"
+
+// import * as encoding from 'text-encoding'
 
 export const fetchAlerts = () => {
   return fetch(process.env.REACT_APP_REST_BASE_URL + '/alerts', {
     method: 'GET',
-    responseType: 'arraybuffer',
     mode: 'cors',
     cache: 'no-cache',
     credentials: 'same-origin',
@@ -17,7 +18,6 @@ export const fetchAlerts = () => {
 export const fetchThreats = () => {
   return fetch(process.env.REACT_APP_REST_BASE_URL + '/threats', {
     method: 'GET',
-    responseType: 'arraybuffer',
     mode: 'cors',
     cache: 'no-cache',
     credentials: 'same-origin',
@@ -30,27 +30,29 @@ export const fetchThreats = () => {
     })
     .then(stream => createArrayBuffer(stream))
     .then(result => {
+
+
       // GET THE LIST FROM THE PROTOCOL BUFFER
-      const UInt8ImageArray =
-        new message.UnknownObjectEntityRepository.deserializeBinary(
-          result,
-        ).getEntityList()
-      setCurrentThreats(() => [...setThreats()])
-      function setThreats() {
-        let threatsArray = []
-        for (let i = 0; i < UInt8ImageArray.length; i++) {
-          console.log(threatsArray)
-          const currentUInt8Id = UInt8ImageArray[i].getId()
-          const currentUInt8Image = UInt8ImageArray[i]
-            .getUnknownobject()
-            .getImage()
-          const iteratedObjectOfAlerts = {
-            id: currentUInt8Id,
-            message: `threat ${currentUInt8Id}`,
-            value: currentUInt8Image,
-          }
-          threatsArray.push(iteratedObjectOfAlerts)
-        }
-        return threatsArray
-      }
+      // const UInt8ImageArray =
+      //   new message.UnknownObjectEntityRepository.deserializeBinary(
+      //     result,
+      //   ).getEntityList()
+      // setCurrentThreats(() => [...setThreats()])
+      // function setThreats() {
+      //   let threatsArray = []
+      //   for (let i = 0; i < UInt8ImageArray.length; i++) {
+      //     console.log(threatsArray)
+      //     const currentUInt8Id = UInt8ImageArray[i].getId()
+      //     const currentUInt8Image = UInt8ImageArray[i]
+      //       .getUnknownobject()
+      //       .getImage()
+      //     const iteratedObjectOfAlerts = {
+      //       id: currentUInt8Id,
+      //       message: `threat ${currentUInt8Id}`,
+      //       value: currentUInt8Image,
+      //     }
+      //     threatsArray.push(iteratedObjectOfAlerts)
+      //   }
+      //   return threatsArray
+  }
 }
